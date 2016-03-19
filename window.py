c163b1ae8a0c3482f618df61588abcaf3183c9e4
TheEulays <ulyssequocanh.avenet@gmail.com>
2016-03-19 17:43:32 +0100

    La majorité des fichiers sont là
    Rajouter les votres !

---
+++ window.py
@@ -0,0 +1,94 @@
+from tkinter import *
+from tkinter import ttk
+from PIL import Image, ImageTk
+
+def loadImage(path):
+    img = Image.open(path)
+    img2 = ImageTk.PhotoImage(img)
+    return img2
+
+def createWindow(keyListener):
+    global root, windowWidth, windowHeight
+    root = Tk()
+    
+    windowWidth = 704
+    windowHeight = 704
+   
+    root.title("AVENTURE Alpha 0.4")#TODO
+    root.resizable(width = FALSE, height = FALSE)
+    root.geometry(str(windowWidth)+ "x" + str(windowHeight))
+    root.bind("<Key>",keyListener)
+
+def gameCanvas():
+    global windowWidth, windowHeight, root
+    mainCanvas = Canvas(root, width = 576 ,height = 576, bg = "black", bd = 0, highlightthickness=0)
+    textCanvas = Canvas(root, width = 704,height = 128, bg = "#39CAFA", bd = 0, highlightthickness=0)
+    menuCanvas = Canvas(root, width = 128,height = 576, bg = "green", bd = 0, highlightthickness=0)
+
+    mainCanvas.place(x = 0, y = 0)
+    textCanvas.place(x = 0, y = 576)
+    menuCanvas.place(x = 576, y = 0)
+
+    root.update()
+    
+    return mainCanvas, textCanvas, menuCanvas
+
+def titleMenu(onPlay, background):
+    global root, windowWidth, windowHeight
+    c = Canvas(root,width = 704, height = 704)
+    c.place(x = 0, y = 0)
+
+    c.create_image(0, 0, anchor = NW, image = background)
+
+    playButton = Button(c, text="PLAY",command = onPlay)
+    quitButton = Button(c, text="EXIT",command = root.destroy)
+
+    #TODO des images
+
+    playButton.place(x = 250, y = 250)
+    quitButton.place(x = 250, y = 350)
+
+    return c
+
+def dialogNext():
+    global label, d, index, root, b, l
+    index += 1
+    if index < len(d):
+        label.config(text = d[index], bg = "#39CAFA")
+        root.update()
+    else:
+        label.destroy()
+        b.destroy()
+        root.update()
+        l()
+        
+def dialog(canvas, dialog, unlock):
+    global label, d, index, b, l
+    l = unlock
+    index = 0
+    d = dialog
+    label = Label(canvas, text = d[index], bg = "#39CAFA")
+    label.place(x = 30, y = 40)
+    button = Button(canvas, text = "OK", command = dialogNext)
+    button.place(x = 30, y = 60)
+    b = button 
+    root.update()
+
+def okbutton():
+    global entry
+    print(entry.get())
+    entry.delete(0,'end')
+
+def questionnaire(canvas):
+    global entry, root
+    entry = Entry(canvas)
+    button = Button(canvas, text = "OK", command = okbutton)
+    entry.place(x = 50, y = 50)
+    button.place(x = 50, y = 100)
+
+def runWindow():
+    root.mainloop()
+
+def destroy():
+    root.destroy()
+
